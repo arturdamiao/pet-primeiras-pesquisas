2509122034492ac28b939e0c66bd04b3e52c6a6a
arturdamiao <arturdamiao@hotmail.com.br>
2025-05-07 11:26:51 -0300

    inserindo testes para análise

--- script/02-eda.R
+++ script/02-eda.R
@@ -1,3 +1,8 @@
+# carregar pacotes
+if(require(BaylorEdPsych)==F)install.packages('BaylorEdPsych');require(BaylorEdPsych)
+if(require(ResourceSelection)==F)install.packages('ResourceSelection');require(ResourceSelection)
+library(sjPlot)
+
 fuvest <- readxl::read_xlsx("dados/fuvest.xlsx")
 View(fuvest)
 
@@ -18,3 +23,26 @@ base::summary(mod)
 
 tab_model(mod)
 
+## ---------------------- Criação de um Modelo Nulo para comparação ------------
+
+# OBS: Em modelos de regressão se compara o modelo proposto com o modelo nulo.
+# modelo nulo: apenas VD e o intercepto
+fit5_rep_null <- glm(formula = reeleito ~ 1, family=binomial(link=logit),
+                     data=dat) 
+
+
+## ---------------------- Tabela 4 - Teste de Hosmer e Lemeshow ----------------
+hoslem.test(fit5_rep$y, fitted(fit5_rep), g=10)
+
+## ---------------------- Tabela 5 - Teste Omnibus dos coeficientes  -----------
+
+# OBS: O SPSS entrega essa tabela como default. No R precisamos calculá-lo. 
+#     Primeiro, precisamos dos graus de liberdade
+
+fit5_rep$df.null-fit5_rep$df.residual # Gl do modelo nulo, menos Gl do nosso 
+# modelo completo.
+
+#     Segundo, deviance do modelo nulo, menos o desvio do modelo completo
+#     Essa diferença se distribui em ChiSquare
+
+with(fit5_rep, null.deviance - deviance)
